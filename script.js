482583fd7c88041b74b3badfd1f11af2c3af0efb
yhaw12 <yooxing11@gmail.com>
2023-02-08 21:28:00 +0000

    update

--- script.js
+++ script.js
@@ -14,5 +14,27 @@ bars.addEventListener('click', ()=>{
     document.querySelector('.fa-times').addEventListener('click', ()=>{
         bars.classList.replace('fa-times', 'fa-bars')   
     });
-});
+})
 
+// Image Slider
+
+// let currentImg = document.getElementById('currentimg');
+
+// let imgArray = [
+//     "pexels-pix (3).jpg",
+//     "images/pexels-pix (2).jpg",
+//     "images/pexels-pix (3).jpg",
+//     "images/pexels-pix (1).jpg",
+//     "images/watch.jpg",
+// ];
+
+// let currentIndex = 0
+
+// setInterval(() => {
+//     currentIndex++    
+//     if (currentImg >= imgArray.length){
+//         currentIndex = 0
+//     }
+   
+//     currentImg.src = imgArray[currentIndex];
+// }, 1000);
