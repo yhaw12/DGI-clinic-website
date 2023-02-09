c2790a4869cbc20eb40ceac7275c679d9a5a5e37
yhaw12 <yooxing11@gmail.com>
2023-02-09 23:41:11 +0000

    another update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // header
 
 window.addEventListener('scroll', ()=>{
-    document.querySelector('.header').classList.toggle('active');
+    document.querySelector('#header').classList.toggle('active');
 });
 
 
@@ -16,25 +16,38 @@ bars.addEventListener('click', ()=>{
     });
 })
 
-// Image Slider
 
-// let currentImg = document.getElementById('currentimg');
 
-// let imgArray = [
-//     "pexels-pix (3).jpg",
-//     "images/pexels-pix (2).jpg",
-//     "images/pexels-pix (3).jpg",
-//     "images/pexels-pix (1).jpg",
-//     "images/watch.jpg",
-// ];
+// select the displayed image
 
-// let currentIndex = 0
 
-// setInterval(() => {
-//     currentIndex++    
-//     if (currentImg >= imgArray.length){
-//         currentIndex = 0
-//     }
-   
-//     currentImg.src = imgArray[currentIndex];
-// }, 1000);
+let bannerImage = document.querySelector('#currentimg');
+
+let currentIndex = 0
+
+let imagesArray = [
+    "images/pexels-pix (2).jpg",
+    "images/pexels-pix (3).jpg",
+]
+
+
+function changeSlider(){
+    currentIndex =+1
+    if (bannerImage > imagesArray.length){
+        currentIndex = 0
+    }
+
+    bannerImage.src = imagesArray[currentIndex];
+};
+
+setInterval(changeSlider(), 1000);
+
+
+
+// footer Date
+
+let footerDate = document.querySelector(".date");
+
+const date = new Date().getFullYear();
+
+footerDate.textContent = date;
